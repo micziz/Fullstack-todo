36578b9ae71acfc49a1bc671099beaa57bd60d3c
micziz <miczicontent@gmail.com>
2022-07-22 18:42:29 +0200

    Initial Commit

---
+++ express.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.sendFile('./app/index.html', { root: __dirname });
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile('./app/about.html', { root: __dirname });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Fullstack Todo
+
+Fullstack Todo built with vanilla and Express.JS
